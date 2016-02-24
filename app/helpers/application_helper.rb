8ed5fcd6aea9ba2af7f91db2a33cfd235b5d2a2f
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2016-02-24 15:14:22 -0800

    Add open graph meta tags so the site shows up nicely on twitter and facebook

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def page_title(title)
-        site_name = "MIT Sustainability"
+        site_name = "MIT Sustainability Summit"
         title.present? ? "#{title} | #{site_name}" : site_name
     end
 end
